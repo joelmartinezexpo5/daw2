55d2ccb599e92659dedaec4e837ddeec271204fa
unknown <daw2@IESMHP.LOCAL>
2024-10-09 19:22:39 +0200

    Composer

---
+++ DWES/PHP/ejemplos/public/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php'; 
+
+use App\Controllers\HolaMundoController; // el namespace
+
+$holaMundoController = new HolaMundoController();
+
+$holaMundoController->index();
+
+echo '<hr>';
+
+echo holaMundo();
