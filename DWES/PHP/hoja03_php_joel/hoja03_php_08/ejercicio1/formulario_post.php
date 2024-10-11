a2336b4276bfa244d9680e462bf6dd52999e276e
unknown <daw2@IESMHP.LOCAL>
2024-10-11 18:20:28 +0200

    PHP

---
+++ DWES/PHP/hoja03_php_joel/hoja03_php_08/ejercicio1/formulario_post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+	<title>Ejemplo Formularios</title> 
+</head> 
+<body>
+	<h1>Ejemplo de procesado de formularios</h1> 
+	<form action="ejemplo1.php" method="post">
+        <label for="nombre">Introduzca su nombre:</label>
+		<input type="text" id="nombre" name="nombre">
+		<br/> 
+        <label for="apellido">Introduzca sus apellidos:</label>
+		<input type="text" id="apellido" name="apellidos"><br/>
+        <br>
+        <label for="edad">Introduzca su edad:</label>
+        <input type="number" id="edad" name="edad"> 
+		<input type="submit" name="enviar" value="Enviar">
+	</form>
+</body> 
+</html> 
+
