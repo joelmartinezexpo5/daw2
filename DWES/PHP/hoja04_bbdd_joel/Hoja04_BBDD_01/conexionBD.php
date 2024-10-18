2dfdc5f64c92a1a8f893b9649e7497613625b72b
unknown <daw2@IESMHP.LOCAL>
2024-10-18 20:10:37 +0200

    Ejercicio 1 BBDD

---
+++ DWES/PHP/hoja04_bbdd_joel/Hoja04_BBDD_01/conexionBD.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+final class ConnectionPDODotenv
+{
+    private static ?PDO $connection = null;
+
+    final private function __construct() {}
+
+    final public static function getConnection(): ?PDO
+    {
+        try {
+            if ( ! self::$connection) {
+                self::$connection = new PDO(
+                    dsn: $_ENV['DB_DSN'],
+                    username: $_ENV['DB_USERNAME'],
+                    password: $_ENV['DB_PASSWORD'],
+                );
+            }
+        } catch (PDOException $e) {
+            echo match ($e->getCode()) {
+                1049 => 'Base de datos no encontrada',
+                1045 => 'Acceso denegado',
+                2002 => 'Conexión rechazada',
+                default => 'Error desconocido',
+            };
+        }
+
+        return self::$connection;
+    }
+     private function __clone() {}
+}
+
+$connection = ConnectionPDODotenv::getConnection();
+
+if ($connection instanceof PDO)
+{
+    echo 'Conexión establecida correctamente';
+}
