b92ae56e5ed81460633a54e6666a7ee2e802c739
unknown <daw2@IESMHP.LOCAL>
2024-10-25 20:03:11 +0200

    Corregido

--- DWES/EXAJoelMartinez - copia/Ejercicio3/index.php
+++ DWES/EXAJoelMartinez - copia/Ejercicio3/index.php
@@ -1,31 +1,3 @@
-<?php
-    require_once 'vendor/autoload.php';
-
-    $faker = Faker\Factory::create('es_ES');
-
-    if(isset($_GET['numNombre'])){
-        echo '<h2>Resultados</h2>';
-        echo '<h3>Nombres</h3>';
-        $numero=$_GET['numNombre'];
-        for($i = 0; $i < $numero; ++$i){
-            echo $faker->name().'<br>';
-        }
-    }
-    if(isset($_GET['numEmail'])){
-        echo '<h3>Emails</h3>';
-        $numero=$_GET['numEmail'];
-        for($i = 0; $i < $numero; ++$i){
-            echo $faker->email().'<br>';
-        }
-    }
-    if(isset($_GET['numTelefono'])){
-        echo '<h3>Telefonos</h3>';
-        $numero=$_GET['numTelefono'];
-        for($i = 0; $i < $numero; ++$i){
-            echo $faker->mobileNumber().'<br>';
-        }
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,15 +7,15 @@
 </head>
 <body>
     <h1>Generar Datos Falsos con Faker</h1>
-    <form action="" method="GET">
+    <form action="" method="POST">
         <label for="numNombre">Numero de Nombres:</label>
-        <input type="text" name="numNombre">
+        <input type="number" name="numNombre" value="<?php if(isset($_POST['numNombre'])){echo $_POST['numNombre'];}else{echo 0;} ?>">
         <br>
         <label for="numEmail">Numero de Emails</label>
-        <input type="email" name="numEmail">
+        <input type="number" name="numEmail" value="<?php if(isset($_POST['numEmail'])){echo $_POST['numEmail'];}else{echo 0;} ?>">
         <br>
         <label for="numTelefono">Numero de telefonos moviles</label>
-        <input type="number" name="numTelefono">
+        <input type="number" name="numTelefono" value="<?php if(isset($_POST['numTelefono'])){echo $_POST['numTelefono'];}else{echo 0;} ?>">
         <br>
         <button type="submit">Generar</button>
     </form>
@@ -51,3 +23,38 @@
 
 </body>
 </html>
+
+<?php
+    require_once 'vendor/autoload.php';
+
+    $faker = Faker\Factory::create('es_ES');
+
+    if(isset($_POST['numNombre'])){
+        echo '<h2>Resultados</h2>';
+        echo '<h3>Nombres</h3>';
+        echo '<ul>';
+        $numero=$_POST['numNombre'];
+        for($i = 0; $i < $numero; ++$i){
+            echo '<li>'.$faker->name().'</li>';
+        }
+        echo '</ul>';
+    }
+    if(isset($_POST['numEmail'])){
+        echo '<h3>Emails</h3>';
+        echo '<ul>';
+        $numero=$_POST['numEmail'];
+        for($i = 0; $i < $numero; ++$i){
+            echo '<li>'.$faker->email().'</li>';
+        }
+        echo '</ul>';
+    }
+    if(isset($_POST['numTelefono'])){
+        echo '<h3>Telefonos</h3>';
+        echo '<ul>';
+        $numero=$_POST['numTelefono'];
+        for($i = 0; $i < $numero; ++$i){
+            echo '<li>'.$faker->mobileNumber().'</li>';
+        }
+        echo '</ul>';
+    }
+?>
