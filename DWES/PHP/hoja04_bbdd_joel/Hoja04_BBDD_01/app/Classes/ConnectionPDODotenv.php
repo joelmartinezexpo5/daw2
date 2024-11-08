3b1a598f83ec04171ce0aee885d5cd80cd52d24c
unknown <daw2@IESMHP.LOCAL>
2024-11-08 20:00:57 +0100

    Ejercicio PHP

--- DWES/PHP/hoja04_bbdd_joel/Hoja04_BBDD_01/app/Classes/ConnectionPDODotenv.php
+++ DWES/PHP/hoja04_bbdd_joel/Hoja04_BBDD_01/app/Classes/ConnectionPDODotenv.php
@@ -25,6 +25,7 @@ final class ConnectionPDODotenv
                     username: $_ENV['DB_USERNAME'],
                     password: $_ENV['DB_PASSWORD'],
                 );
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
         } catch (PDOException $e) {
             echo match ($e->getCode()) {
