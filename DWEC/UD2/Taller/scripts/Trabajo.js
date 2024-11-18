8e137a588469bd480e5796b5ecf70bb67a8cc60e
unknown <daw2@IESMHP.LOCAL>
2024-11-18 16:23:48 +0100

    Taller JS

---
+++ DWEC/UD2/Taller/scripts/Trabajo.js
@@ -0,0 +1,9 @@
+class Trabajo{
+    constructor(concepto, precioUnitario, cantidad, totalTrabajo){
+        this.concepto = concepto;
+        this.precioUnitario = precioUnitario;
+        this.cantidad = cantidad;
+        this.totalTrabajo = totalTrabajo;
+    }
+}
+export default Trabajo;
---
+++ DWEC/UD2/Taller/scripts/Vehiculo.js
@@ -0,0 +1,11 @@
+class Vehiculo{
+    constructor(vehiculoId, matricula, marca, modelo, año, motor){
+        this.vehiculoId = vehiculoId;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.motor = motor;
+    }
+}
+export default Vehiculo;
