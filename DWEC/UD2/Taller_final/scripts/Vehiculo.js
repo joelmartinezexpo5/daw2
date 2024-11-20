df9253d3d57cd66731e6e0afdc4e60c961923537
unknown <daw2@IESMHP.LOCAL>
2024-11-20 16:26:25 +0100

    JS

---
+++ DWEC/UD2/Taller_final/scripts/Vehiculo.js
@@ -0,0 +1,13 @@
+class Vehiculo {
+    constructor(vehiculoId, matricula, marca, modelo, año, motor, propietario) {
+        this.vehiculoId = vehiculoId;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.motor = motor;
+        this.propietario = propietario;
+    }
+}
+
+export default Vehiculo;
