35deeb41b6bb45d680f51685836d34edf8e03548
unknown <daw2@IESMHP.LOCAL>
2024-11-22 16:57:51 +0100

    Ejercicio 5

--- DWES/PHP/hoja04_bbdd_joel/Hoja04_BBDD_05/public/turnos.php
+++ DWES/PHP/hoja04_bbdd_joel/Hoja04_BBDD_05/public/turnos.php
@@ -2,15 +2,15 @@
 require_once '../vendor/autoload.php';
 use App\DB;
 
-
 $db = new DB();
 $turnos = $db->getTurnos();
+
 
 echo "<h1>Selecciona un Turno</h1>";
 echo "<form action='' method='post'>";
 echo "<select name='turno_id'>";
 foreach ($turnos as $turno) {
-    echo "<option value='" . $turno['id'] . "'>" . $turno['nombre'] . "</option>";
+    echo "<option value='" . $turno['id'] . "' " . (($turno['id'] == ($_POST['turno_id'] ?? '')) ? "selected" : "") . ">" . $turno['nombre'] . "</option>";
 }
 echo "</select>";
 echo "<input type='submit' value='Mostrar médicos'>";
@@ -25,3 +25,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 }
+
