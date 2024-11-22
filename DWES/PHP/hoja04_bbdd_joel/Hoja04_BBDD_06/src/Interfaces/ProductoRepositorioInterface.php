104465a18cb2d3cf6f8018c82b4dd4d27f5291a7
unknown <daw2@IESMHP.LOCAL>
2024-11-22 20:15:05 +0100

    Hoja 6 PHP

---
+++ DWES/PHP/hoja04_bbdd_joel/Hoja04_BBDD_06/src/Interfaces/ProductoRepositorioInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Interfaces;
+
+interface ProductoRepositorioInterface
+{
+    public function crear(Producto $producto);
+
+    // public function listar();
+
+    // public function listarPorId(int $id);
+
+    // public function borrar(int $id);
+}
+?>
