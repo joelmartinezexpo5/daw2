5ded47893d661af34d7a07cd32e02ce4c1ff1d9a
unknown <daw2@IESMHP.LOCAL>
2024-11-28 17:29:59 +0100

    Repaso y Hoja 7 DIW

--- DWES/PHP/hoja04_bbdd_joel/repaso/hoja1BD/public/index.php
+++ DWES/PHP/hoja04_bbdd_joel/repaso/hoja1BD/public/index.php
@@ -3,47 +3,53 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\FuncionesBD;
 
-$equipos  = FuncionesBD::getEquipos();
-if(isset($_POST['equipo'])){
+$equipos = FuncionesBD::getEquipos();
+if (isset($_POST['equipo'])) {
     $jugadores = FuncionesBD::getJugadoresPorEquipo($_POST['equipo']);
+    $pesos = FuncionesBD::getPesos($_POST['equipo']);
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Jugadores de la NBA</h1>
     <hr>
     <form action="" method="post">
         <label for="equipo">Equipo:</label>
         <select name="equipo" id="equipo">
-            <?php foreach($equipos as $equipo): ?>
-                <option value="<?= htmlspecialchars($equipo)?>" <?= isset($_POST['equipo']) ? 'selected' : ''?>>
-                    <?= htmlspecialchars($equipo)?>
+            <?php foreach ($equipos as $equipo): ?>
+                <option value="<?= htmlspecialchars($equipo) ?>" <?= isset($_POST['equipo']) ? 'selected' : '' ?>>
+                    <?= htmlspecialchars($equipo) ?>
                 </option>
             <?php endforeach; ?>
         </select>
         <button type="submit">Mostrar</button>
     </form>
 
-    <?php if(!empty($jugadores)): ?>
-    <h2>Jugadores del equipo</h2>
-    <table>
-        <tr>
-            <th>Nombre del Jugador</th>
-        </tr>
-        <?php foreach ($jugadores as $jugador): ?>
+    <?php if (!empty($jugadores)): ?>
+        <h2>Jugadores del equipo</h2>
+        <table border="1px solid black">
             <tr>
-                <td><?=htmlspecialchars($jugador)?></td>
+                <th>Nombre del Jugador</th>
+                <th>Peso</th>
             </tr>
+            <?php foreach ($jugadores as $index => $jugador): ?>
+                <tr>
+                    <td><?= htmlspecialchars($jugador) ?></td>
+                    <td><?= $pesos[$index] ?></td>
+                </tr>
             <?php endforeach; ?>
-    </table>
+        </table>
     <?php endif; ?>
 </body>
+
 </html>
