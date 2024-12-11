eac8dfc4c4562a0d97fd7c95195c3ea2b4c9b641
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2024-12-11 16:44:35 +0100

    Formulario Dinamico

--- DWEC/UD3/formularioDinamico/scripts/app.js
+++ DWEC/UD3/formularioDinamico/scripts/app.js
@@ -5,17 +5,46 @@ const tablaDerecha = document.getElementById("tabla-derecha");
 const ciclo = document.getElementById("ciclo");
 const moverIzquierda = document.getElementById("mover-izquierda");
 const moverDerecha = document.getElementById("mover-derecha");
-const enviar = document.getElementById("enviar")
+const enviar = document.getElementById("enviar");
 const resultado = document.getElementById("resultado");
 
-function crearTabla(tabla, datos){
+
+function crearTabla(tabla, datos) {
     tabla.innerHTML = '';
-    datos.forEach(alumnos => {
+    datos.forEach(alumno => {
         const fila = document.createElement('div');
         fila.className = 'fila';
-        fila.textContent = `${alumnos.nombre} (${alumnos.ciclo})`
-        tabla.appendChild('fila')
+        fila.textContent = `${alumno.nombre} (${alumno.ciclo})`;
+        
+        fila.addEventListener('click', () => {
+            fila.classList.toggle('seleccionada');
+        });
+
+        tabla.appendChild(fila);
     });
 }
+
+function filtrarPorCurso() {
+    const cicloSeleccionado = ciclo.value; 
+    return alumnos.filter(alumno => 
+        (cicloSeleccionado === 'Todos' || alumno.ciclo === cicloSeleccionado)
+    );
+}
+
+moverDerecha.addEventListener('click', () => {
+    let seleccionados = tablaIzquierda.getElementsByClassName('seleccionada');
+    let alumnosSeleccionados = [];
+    for(const seleccion of seleccionados){
+        console.log(seleccion.innerText);
+        alumnosSeleccionados.push(seleccion.innerText);
+    }
+    console.log(alumnosSeleccionados);
+    crearTabla(tablaDerecha, alumnosSeleccionados);
+})
+
+ciclo.addEventListener('change', () => {
+    const alumnosFiltrados = filtrarPorCurso();
+    crearTabla(tablaIzquierda, alumnosFiltrados);
+});
 
 crearTabla(tablaIzquierda, alumnos);
