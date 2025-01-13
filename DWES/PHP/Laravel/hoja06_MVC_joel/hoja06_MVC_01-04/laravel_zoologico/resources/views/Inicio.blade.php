eab6227516ac2f5de4dc3f6350f8086af5a2e60a
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-01-13 20:07:49 +0100

    Hoja06 Laravel

---
+++ DWES/PHP/Laravel/hoja06_MVC_joel/hoja06_MVC_01-04/laravel_zoologico/resources/views/Inicio.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head @vite('resources/css/app.css')>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline">Pagina principal</h1>
+</body>
+</html>
