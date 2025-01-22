8a7d89b033eb057417fad9febb50cb3fffc6be81
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-01-22 16:23:59 +0100

    UD4 React DWEC

---
+++ DWEC/UD4/01-prueba/src/components/Hijo.jsx
@@ -0,0 +1,14 @@
+function Hijo({ nombre = "adoptado", fnllorar }) {
+
+    function handleClick() {
+        fnllorar(`${nombre} dice: no cole`);
+    }
+
+    return (
+        <>
+            <h3>Soy {nombre}</h3>
+            <button onClick={handleClick}>Llorar a papa</button>
+        </>
+    );
+}
+export default Hijo;
