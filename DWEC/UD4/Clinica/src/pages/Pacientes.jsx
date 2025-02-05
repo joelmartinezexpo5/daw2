8b13f8d7011abbc8b788b83efee6611517ad5508
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-02-05 17:46:33 +0100

    Clinica

--- DWEC/UD4/Clinica/src/pages/Pacientes.jsx
+++ DWEC/UD4/Clinica/src/pages/Pacientes.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import $negocio from "../services/negocio";
-
+import datos from "../services/datos";
 const Pacientes = () => {
   const [pacientes, setPacientes] = useState([]);
   const [filtro, setFiltro] = useState("");
