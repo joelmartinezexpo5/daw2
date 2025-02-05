eac61fd770a826d2bb0f937cf679c35e4c46468a
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-02-05 16:25:27 +0100

    Clinica

--- DWEC/UD4/Clinica/src/pages/Usuarios.jsx
+++ DWEC/UD4/Clinica/src/pages/Usuarios.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import datos from "../services/datos";
+
 function Usuarios(){
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(() => {
+        setUsuarios(datos.usuarios);
+    });
+
+
+
     return(
         <>
-            <h1>Pagina Usuario</h1>
+            <div>
+                <h2>Listado de Usuarios</h2>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>Password</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usuarios.map((usuario) => (
+                            <tr>
+                                <td>{usuario.id}</td>
+                                <td>{usuario.username}</td>
+                                <td>{usuario.password}</td>
+                                <td>{usuario.tipo}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
