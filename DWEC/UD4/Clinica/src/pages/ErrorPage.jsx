7cda2bdabc40e1a63b26ebbe76815768a55187cc
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-02-09 02:44:13 +0100

    Clinica

--- DWEC/UD4/Clinica/src/pages/ErrorPage.jsx
+++ DWEC/UD4/Clinica/src/pages/ErrorPage.jsx
@@ -1,8 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function ErrorPage(){
-    return(
-        <>
-            <h1>Pagina de error</h1>
-        </>
-    )
-}
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1 style={{ fontSize: '3em', color: '#ff4d4d' }}>¡Vaya! Página no encontrada</h1>
+      <p style={{ fontSize: '1.2em' }}>La página que estás buscando no existe o ha sido movida.</p>
+      <p>
+        <Link to="/" style={{ fontSize: '1.5em', textDecoration: 'underline', color: '#007bff' }}>
+          Volver al inicio
+        </Link>
+      </p>
+    </div>
+  );
+};
+
 export default ErrorPage;
