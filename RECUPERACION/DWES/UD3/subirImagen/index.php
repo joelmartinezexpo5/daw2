eb1e7965634baf2bc1970f8b810ca13cc7f8823c
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-03-31 20:03:10 +0200

    Suibir imagen terminado

--- RECUPERACION/DWES/UD3/subirImagen/index.php
+++ RECUPERACION/DWES/UD3/subirImagen/index.php
@@ -15,13 +15,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Subir Archivo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <form action="" method="post" enctype="multipart/form-data">
+<form action="" method="post" enctype="multipart/form-data">
         <label>Nombre:</label>
         <input type="text" name="nombre" required>
         <br>
