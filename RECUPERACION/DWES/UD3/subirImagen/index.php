12f0b31eadca73a01d6cc8ddc6989798612fbde5
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-03-31 19:46:48 +0200

    Subir imagen terminado

--- RECUPERACION/DWES/UD3/subirImagen/index.php
+++ RECUPERACION/DWES/UD3/subirImagen/index.php
@@ -2,23 +2,18 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $directorioSubida = "images/";
     
-    if (!file_exists($directorioSubida)) {
-        mkdir($directorioSubida);
-    }
-    
-    $nombre = $_POST["name"];
-    $extensionArchivo = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
+    $nombre = $_POST["nombre"];
+    $extensionArchivo = pathinfo($_FILES["archivo"]["name"], PATHINFO_EXTENSION);
     $nombreArchivo = $nombre . "." . $extensionArchivo;
     $rutaArchivo = $directorioSubida . $nombreArchivo;
     
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $rutaArchivo)) {
+    if (move_uploaded_file($_FILES["archivo"]["tmp_name"], $rutaArchivo)) {
         echo "Archivo subido con éxito como $nombreArchivo.";
     } else {
         echo "Error al subir el archivo.";
     }
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -28,12 +23,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <form action="" method="post" enctype="multipart/form-data">
         <label>Nombre:</label>
-        <input type="text" name="name" required>
+        <input type="text" name="nombre" required>
         <br>
         <label>Archivo:</label>
-        <input type="file" name="file" required>
+        <input type="file" name="archivo" required>
         <br>
         <input type="submit" value="Subir">
     </form>
+    <?php
+    $archivos = array_diff(scandir('images/'), array('.','..'));
+    if(count($archivos) > 0){
+        foreach($archivos as $archivo){
+            $extension = pathinfo($archivo, PATHINFO_EXTENSION); 
+            if($extension == 'jpeg' || $extension == 'jpg' || $extension == 'png'){
+                echo "<h1>{$archivo}</h1>";
+                echo "<img src='images/{$archivo}' width='300' height='100px'><br>";
+            }else{
+                echo "<h1>{$archivo}</h1>";
+                echo "<a href=images/{$archivo} download={$archivo}>Descargar archivo</a>";
+            }
+        }
+    }
+    ?>
 </body>
 </html>
