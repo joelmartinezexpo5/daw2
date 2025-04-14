31af8ca32b892a4c858169069e38112659798c24
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-04-14 19:23:33 +0200

    Ej 3 PHP

---
+++ RECUPERACION/DWES/UD4/Hoja04_BBDD_03/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestión Funicular de Bulnes</title>
+</head>
+<body>
+    <h1>Bienvenido al sistema del Funicular de Bulnes</h1>
+    <ul>
+        <li><a href="reserva.php">Reservar plaza</a></li>
+        <li><a href="llegada.php">Llegada al destino</a></li>
+        <li><a href="plazas.php">Gestión de plazas</a></li>
+    </ul>
+</body>
+</html>
