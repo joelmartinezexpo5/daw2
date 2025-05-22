cb370852ae5a32eee0d7df5b6f5465caded5029c
Joel Martínez Expósito <DAW217@iesmhp.local>
2025-05-22 15:45:51 +0200

    Laravel

---
+++ RECUPERACION/DWES/Laravel/locurote/locurilla/familias.sql
@@ -0,0 +1,8 @@
+private $familias = [
+        ['codigo' => 'BBDD', 'nombre' => 'Bases de datos'],
+        ['codigo' => 'Seguridad', 'nombre' =>'Seguridad informática'],
+        ['codigo' => 'Programacion', 'nombre' => 'Programación y desarrollo del software'],
+        ['codigo' => 'Redes', 'nombre' => 'Redes y administración de sistemas'],
+        ['codigo' => 'Sistemas', 'nombre' => 'Sistemas operativos'],
+        ['codigo' => 'Otros', 'nombre' => 'Otros']
+    ];
