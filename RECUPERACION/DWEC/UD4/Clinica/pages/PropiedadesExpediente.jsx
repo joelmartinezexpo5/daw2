0c34fb7ec5c60511ff8887e784fb5d25d0315d46
joelmartinezexpo5 <joelmartinezexpo@gmail.com>
2025-06-04 16:24:37 +0200

    Clinica React

--- RECUPERACION/DWEC/UD4/Clinica/pages/PropiedadesExpediente.jsx
+++ RECUPERACION/DWEC/UD4/Clinica/pages/PropiedadesExpediente.jsx
@@ -1,4 +1,10 @@
+import { useNavigate, useParams } from "react-router-dom";
+
 function PropiedadesExpediente(){
+    const { id } = useParams();
+    const navegar = useNavigate();
+    
+
     return(
         <>
             <h1>Propiedades Expediente</h1>
